0cd0b23beff81a7047c6e8335ab1390cdc838f8d
ffekirnew <ffekirnew0808@gmail.com>
2022-12-17 21:16:27 +0300

    added the function for mod

--- monty.h
+++ monty.h
@@ -78,4 +78,5 @@ void f_stack(stack_t **head, unsigned int counter);
 void f_sub(stack_t **head, unsigned int counter);
 void f_div(stack_t **head, unsigned int counter);
 void f_mul(stack_t **head, unsigned int counter);
+void f_mod(stack_t **head, unsigned int counter);
 #endif
