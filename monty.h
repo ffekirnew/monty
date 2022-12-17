c8102e8288c2c45cb64a1679d30c95b95b6bc694
ffekirnew <ffekirnew0808@gmail.com>
2022-12-17 21:12:34 +0300

    added the function for div

--- monty.h
+++ monty.h
@@ -76,4 +76,5 @@ void addqueue(stack_t **head, int n);
 void f_queue(stack_t **head, unsigned int counter);
 void f_stack(stack_t **head, unsigned int counter);
 void f_sub(stack_t **head, unsigned int counter);
+void f_div(stack_t **head, unsigned int counter);
 #endif
